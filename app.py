3f0f293659fbe2e01f4c4fb6d6022e10396ac58a
onceuponapril <31635476+onceuponapril@users.noreply.github.com>
2018-09-10 12:40:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template,request,redirect,jsonify,url_for
 import digitSumCtrl
-import csv
+
 app = Flask(__name__)
 app.static_folder = 'static'
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # return redirect('/result')
         userInput=request.form['input']
         extractDigit=digitSumCtrl.ExtractDigit(userInput)
         userOutput=extractDigit.sumInput()
